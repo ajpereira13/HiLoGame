f7cd8ef3529e57afb780cf5c95404e01efaf8710
ajpereira13 <a.j.pereira13@hotmail.com>
2016-04-18 21:57:04 -0400

    Commit

--- src/game/HiLoLogic2.java
+++ src/game/HiLoLogic2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class HiLoLogic2 {
 	
+	static Scanner scan = new Scanner(System.in);
 	static int rand = (int) (100 * Math.random() + 1);
 	static String s = new String();
 	static boolean stillPlaying = true;
@@ -12,27 +13,16 @@ public class HiLoLogic2 {
 	
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		
 		System.out.println("Take a guess, or quit at any time by entering \"q\".");
 		do {
 			s = scan.nextLine();
 			count++;
 			HiLo(s);
-			if (stillPlaying == false) {
-				System.out.println("Thank you for playing.");
-				System.out.println("You made " + count + " guesses.");
-				System.out.println();
-				System.out.println("If you would like to play again press \"y\".");
-				s = scan.nextLine();
-				if (s.equals("y")) {
-					stillPlaying = true;
-					rand = (int) (100 * Math.random() + 1);
-					count = 0;
-					System.out.println("Take a guess, or quit at any time by entering \"q\".");
-				}
-			}
+			
 		} while (!s.equals("q") && stillPlaying);
 		scan.close();
+		System.out.println("Thank you for playing.");
 		
 	}
 	
@@ -41,13 +31,29 @@ public class HiLoLogic2 {
 		if (s.matches("\\d+")) {
 			n = Integer.parseInt(s);
 			if (n == rand) {
-				stillPlaying = false;
 				System.out.println("You guessed correctly!");
-			} if (n > rand && n < 101 ) {
+				if (count == 1) {
+					System.out.println("You made " + count + " guess.");
+				} else {
+					System.out.println("You made " + count + " guesses.");
+				}
+				System.out.println();
+				System.out.println("If you would like to play again press \"y\", otherwise enter any charcter.");
+				s = scan.nextLine();
+				if (s.equals("y")) {
+					stillPlaying = true;
+					rand = (int) (100 * Math.random() + 1);
+					count = 0;
+					n = -1;
+					System.out.println("Take a guess, or quit at any time by entering \"q\".");
+				} else {
+					stillPlaying = false;
+				}
+			} else if (n > rand && n < 101 ) {
 				System.out.println("Your guess is too high.");
-			} if (n < rand && n > 0) {
+			} else if (n < rand && n > 0) {
 				System.out.println("Your guess is too low.");
-			} if (n < 1 || n > 100) {
+			} else {
 				System.out.println("Your guess must be an integer on the interval [1, 100]");
 			}
 		} else if (s.equals("q")) {
